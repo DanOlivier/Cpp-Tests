572e6ab561bba0938c2cca96b12839826b54cadd
Daniel Olivier <dan_olivier@hotmail.com>
2018-03-09 15:48:00 -0500

    CyclicRotation to support negative shift

--- CyclicRotation.cpp
+++ CyclicRotation.cpp
@@ -5,15 +5,19 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> &A, uint K)
+vector<int> solution(vector<int> &A, int K)
 {
-	if(K >= A.size())
-	{
-		stringstream os; 
-		os << "Rotation factor (" << K << ") must be smaller than size of vector (" << A.size() << ")!";
-		throw runtime_error(os.str());
-	}
+	// Need to truncate to not rotate over the size of the the vector?
+	if(K > 0)
+		K = K % A.size();
+	if(K < 0)
+		K = -(-K % A.size());
+	if(K == 0)
+		return A;
+
 	vector<int> B(A.size());
+	if(K < 0)
+		K = A.size() + K;
 	copy(A.end()-K, A.end(), B.begin());
 	copy(A.begin(), A.end()-K, B.begin()+K);
 	return B;
@@ -24,24 +28,65 @@ TEST(CyclicRotation, Trivial)
 	{
 		vector<int> a{ 9, 7 }, b{ 7, 9 };
 		EXPECT_EQ(solution(a, 1), b);
+		EXPECT_EQ(solution(a, 2), a);
+		EXPECT_EQ(solution(a, 3), b);
 	}
+	{
+		vector<int> 
+			e{ 9, 7, 6 }, 
+			f{ 6, 9, 7 }, 
+			g{ 7, 6, 9 };
+		EXPECT_EQ(solution(e, 1), f);
+		EXPECT_EQ(solution(e, 2), g);
+		EXPECT_EQ(solution(e, 3), e);
+		EXPECT_EQ(solution(e, 4), f);
+	}
+	{
+		vector<int> 
+			a{ 3, 8, 9, 7, 6 }, 
+			b{ 6, 3, 8, 9, 7 }, 
+			c{ 7, 6, 3, 8, 9 }, 
+			d{ 9, 7, 6, 3, 8 },
+			e{ 8, 9, 7, 6, 3 };
+		EXPECT_EQ(solution(a, 1), b);
+		EXPECT_EQ(solution(a, 2), c);
+		EXPECT_EQ(solution(a, 3), d);
+		EXPECT_EQ(solution(a, 4), e);
+		EXPECT_EQ(solution(a, 5), a);
+		EXPECT_EQ(solution(a, 6), b);
+	}
+}
+
+TEST(CyclicRotation, Negative)
+{
 	{
 		vector<int> a{ 9, 7 }, b{ 7, 9 };
-		ASSERT_THROW(solution(a, 2), runtime_error);
+		EXPECT_EQ(solution(a, -1), b);
+		EXPECT_EQ(solution(a, -2), a);
+		EXPECT_EQ(solution(a, -3), b);
 	}
 	{
-		vector<int> e{ 9, 7, 6 }, f{ 6, 9, 7 };
-		EXPECT_EQ(solution(e, 1), f);
+		vector<int> 
+			e{ 9, 7, 6 }, 
+			f{ 7, 6, 9 }, 
+			g{ 6, 9, 7 };
+		EXPECT_EQ(solution(e, -1), f);
+		EXPECT_EQ(solution(e, -2), g);
+		EXPECT_EQ(solution(e, -3), e);
+		EXPECT_EQ(solution(e, -4), f);
 	}
 	{
-		vector<int> g{ 9, 7, 6 }, h{ 7, 6, 9 };
-		EXPECT_EQ(solution(g, 2), h);
+		vector<int> 
+			a{ 3, 8, 9, 7, 6 }, 
+			b{ 8, 9, 7, 6, 3 }, 
+			c{ 9, 7, 6, 3, 8 },
+			d{ 7, 6, 3, 8, 9 },
+			e{ 6, 3, 8, 9, 7 };
+		EXPECT_EQ(solution(a, -1), b);
+		EXPECT_EQ(solution(a, -2), c);
+		EXPECT_EQ(solution(a, -3), d);
+		EXPECT_EQ(solution(a, -4), e);
+		EXPECT_EQ(solution(a, -5), a);
+		EXPECT_EQ(solution(a, -6), b);
 	}
-	{
-		vector<int> k{ 3, 8, 9, 7, 6 }, l{ 9, 7, 6, 3, 8 };
-		EXPECT_EQ(solution(k, 3), l);
-	}
-	{
-		vector<int> k{ 3, 8, 9, 7, 6 }, l{ 8, 9, 7, 6, 3 };
-		EXPECT_EQ(solution(k, 4), l);
-	}}
+}
