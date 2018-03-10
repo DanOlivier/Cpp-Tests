0c760039d329f76ec54f81b5c6f397bbc12b10ab
Daniel Olivier <dan_olivier@hotmail.com>
2018-03-10 11:44:53 -0500

    PermMissingElement

--- 03-Time-Complexity/PermMissingElement.cpp
+++ 03-Time-Complexity/PermMissingElement.cpp
@@ -2,26 +2,24 @@
 #include <cstdlib>      // std::rand, std::srand
 #include <algorithm>
 #include <numeric>
+#include <vector>
 
 using namespace std;
 
-int solution(int A[], int N)
+int solution(const vector<int>& A)
 {
 	ulong sum = 0;
-	for(int i = 0; i < N; i++)
+	uint N = A.size();
+	for(uint i = 0; i < N; i++)
 	{
 		sum += A[i];
 	}
 	ulong n = N+1;
 	ulong expectedSum = n*(1 + n)/2;
-	cout << "expectedSum: " << expectedSum << endl;
-	cout << "sum:         " << sum << endl;
+	//cout << "expectedSum: " << expectedSum << endl;
+	//cout << "sum:         " << sum << endl;
+	//cout << "diff:        " << (expectedSum - sum) << endl;
 	return expectedSum - sum;
-}
-
-int solution(vector<int> &A)
-{
-	return solution(&A[0], A.size());
 }
 
 int myrandom (int i) { return std::rand()%i; }
@@ -39,20 +37,12 @@ TEST(PermMissingElem, Trivial)
 {
 	srand ( unsigned ( time(0) ) );
   
+	EXPECT_EQ(solution(vector<int>{ 2, 3, 1, 5 }), 4);
+	EXPECT_EQ(solution(vector<int>{ 2, 3, 1, 5, 6, 9, 4, 10, 8, 11}), 7);
+	EXPECT_EQ(solution(vector<int>{ 2, 3, 1, 5, 9, 7, 12, 4, 10, 8, 11}), 6);
+	for(int i = 0; i < 10000; ++i)
 	{
-		vector<int> a{ 2, 3, 1, 5 };
-		EXPECT_EQ(solution(a), 4);
-	}
-	{
-		vector<int> a{ 2, 3, 1, 5, 6, 9, 4, 10, 8, 11};
-		EXPECT_EQ(solution(a), 7);
-	}
-	{
-		vector<int> a{ 2, 3, 1, 5, 9, 7, 12, 4, 10, 8, 11};
-		EXPECT_EQ(solution(a), 6);
-	}
-	{
-		vector<int> a(10);
+		vector<int> a(myrandom(100000)+1);
 		iota(a.begin(), a.end(), 1);
 		vector<int>::iterator it = a.begin() + myrandom(a.size());
 		int saved = *it;
@@ -60,7 +50,7 @@ TEST(PermMissingElem, Trivial)
 		a.shrink_to_fit();
 		std::random_shuffle( a.begin(), a.end() );
 		//printv(a);
-		cout << "saved: " << saved << endl;
+		//cout << "saved: " << saved << endl;
 
 		EXPECT_EQ(solution(a), saved);
 	}
