790d964c7c195d5d037b09fa654cb8e1ad3501dd
Daniel Olivier <dan_olivier@hotmail.com>
2018-03-14 11:07:48 -0400

    Q4 optimal

--- Unity-Test/Q4-tests.cpp
+++ Unity-Test/Q4-tests.cpp
@@ -22,37 +22,34 @@ vector<int> GenerateTestVector(uint M, uint N)
 {
     random_device rd;
     mt19937 g(rd());
-    // set S contains unique numbers corresponding to destinations
-    // we want just one single valid and complete sequence A (of M elements)
-    // so we create a vector of N-M elements, filled from and array of M-1 unique numbers
-    // and then add a single unique number (completing array M), and
-    // add it to a random location in the original vector A
-    unordered_set<int> S;
-    uniform_int_distribution<int> dist{1, int(3*M)};
-    for(; S.size() < M-1;) { S.insert(dist(g)); }
-    
-    //vector<int> S2(S.size());
-    //for(auto& s: S) { s2.push_back(); } 
-    //shuffle(S2.begin(), S2.end(), g);
+    // vector S contains unique numbers (corresponding to "destinations")
+    // we want just one subsequence of S (of size M) in A (of N elements)
+    // 1. filled array S with M-1 unique (sequentially numbered) numbers
+    // 1. create a vector A of N-M elements, and fill it with random elements from S
+    // 2. add a single unique number to the end of S (now with size M), and
+    // 3. now add S to a random location in vector A (now with size N)
+    //unordered_set<int> S;
+    //uniform_int_distribution<int> dist{int(-3*M), int(3*M)};
+    //for(; S.size() < M-1;) { S.insert(dist(g)); }
+
+    vector<int> S;
+    uint j = 0;
+    for(; j < M-1; ++j) { S.push_back(j); }
+    shuffle(S.begin(), S.end(), g);
 
     vector<int> A; A.reserve(N);
     uniform_int_distribution<int> dist2{0, int(S.size())-1};
     for(uint i = 0; i < N-M; ++i) 
     {
         uint k = dist2(g);
-        auto s = S.begin();
-        for(uint j = 0; j < k; ++j) 
-            ++s;
+        auto s = S.begin() + k;
+        //auto s = S.begin(); for(uint j = 0; j < k; ++j) { ++s; }
         A.push_back(*s);
     }
 
     // add the last unique element
-    for(; S.size() < M;) { S.insert(dist(g)); }
-
-    // re-create the vector of elements
-    //S2.clear();
-    //for(auto& s: S) { S2.push_back(); } 
-    //shuffle(S2.begin(), S2.end(), g);
+    //for(; S.size() < M;) { S.insert(dist(g)); }
+    S.push_back(j);
 
     uniform_int_distribution<int> dist3{0, int(N-M)};
     vector<int>::iterator it = A.begin() + dist3(g);
@@ -60,16 +57,23 @@ vector<int> GenerateTestVector(uint M, uint N)
     return A;
 }
 
+ostream_iterator<uint> out_it (cout,", ");
+
 TEST(Q4, Random)
 {
-    vector<int> A(GenerateTestVector(10, 100));
-    EXPECT_EQ(solution(A), 10);
-
-    A = GenerateTestVector(100, 1000);
-    EXPECT_EQ(solution(A), 100);
-
-    A = GenerateTestVector(1000, 10000);
-    EXPECT_EQ(solution(A), 1000);
+    for(int i = 0; i < 100; ++i)
+    {
+        vector<int> A = GenerateTestVector(10, 20);
+        EXPECT_EQ(solution(A), 10);
+        if(solution(A) != 10)
+        {
+            cout << "Failed vector: ";
+            copy( A.begin(), A.end(), out_it ); cout << endl;
+        }
+    }
+    EXPECT_EQ(solution(GenerateTestVector(10, 100)), 10);
+    EXPECT_EQ(solution(GenerateTestVector(100, 1000)), 100);
+    EXPECT_EQ(solution(GenerateTestVector(1000, 1E4)), 1000);
 }
 
 TEST(Q4, LargeVectorSize)
@@ -77,12 +81,18 @@ TEST(Q4, LargeVectorSize)
     EXPECT_EQ(solution(GenerateTestVector(100, 1E5)), 100);
     EXPECT_EQ(solution(GenerateTestVector(100, 1E6)), 100);
     EXPECT_EQ(solution(GenerateTestVector(100, 1E7)), 100);
-    EXPECT_EQ(solution(GenerateTestVector(100, 1E8)), 100);
+    //EXPECT_EQ(solution(GenerateTestVector(100, 1E8)), 100);
 }
 
 TEST(Q4, LargeKeySize)
 {
-    EXPECT_EQ(solution(GenerateTestVector(100, 10000)), 100);
-    EXPECT_EQ(solution(GenerateTestVector(1000, 10000)), 1000);
-    EXPECT_EQ(solution(GenerateTestVector(5000, 10000)), 5000);
+    EXPECT_EQ(solution(GenerateTestVector(100, 1E4)), 100);
+    EXPECT_EQ(solution(GenerateTestVector(1000, 1E4)), 1000);
+    EXPECT_EQ(solution(GenerateTestVector(5000, 1E4)), 5000);
+    EXPECT_EQ(solution(GenerateTestVector(1E4, 2E4)), 1E4);
+    EXPECT_EQ(solution(GenerateTestVector(1E4, 1E5)), 1E4);
+    EXPECT_EQ(solution(GenerateTestVector(5E4, 1E5)), 5E4);
+    EXPECT_EQ(solution(GenerateTestVector(1E5, 2E5)), 1E5);
+    EXPECT_EQ(solution(GenerateTestVector(5E5, 2E6)), 5E5);
+    //EXPECT_EQ(solution(GenerateTestVector(5E6, 1E7)), 5E6);
 }
--- Unity-Test/Q4.cpp
+++ Unity-Test/Q4.cpp
@@ -1,10 +1,28 @@
 #include <vector>
-#include <set>
-#include <deque>
 //#include <algorithm>
-#include <unordered_set>
+#include <set>
+#include <unordered_map>
 
 using namespace std;
+
+template <typename K>
+class CountedMap
+{
+    std::unordered_map<K, uint> m;
+public:
+    CountedMap() {}
+    void add(K i) { m[i]++; }
+    void remove(K i) {
+        unordered_map<int, uint>::iterator it = m.find(i);
+        if(it != m.end())
+        {
+            it->second--;
+            if(it->second < 1)
+                m.erase(it);
+        }
+    }
+    size_t size() { return m.size(); }
+};
 
 int solution(const vector<int> &A)
 {
@@ -19,24 +37,27 @@ int solution(const vector<int> &A)
     };
     uint sizeS = f();
     
-    unordered_set<int> S; // value to index
-    deque<int> V;
+    CountedMap<int> S;
 
     uint min_days = N;
     uint start = 0;
     for(uint i = 0; i < N; i++)
     {
-        V.push_front(A[i]);
-        S.insert(A[i]);
-redo:
-        if(S.size() == sizeS) // The set is filled, and all destinations have been visited
+        if(i-start+1 > min_days) // we don't consider solutions larger than the current one
         {
-            min_days = min(min_days, (uint)V.size());
-            V.pop_back();
-            S.clear();
-            for(auto a: V) { S.insert(a); }
+            S.remove(A[start]);
             start++;
-            goto redo;
+        }
+        S.add(A[i]);
+
+        while(S.size() == sizeS) // The set is filled, and all destinations have been visited
+        {
+            min_days = min(min_days, (uint)i-start+1);
+            if(min_days == sizeS)
+                return sizeS; // optimal
+            // shrink the window from the start
+            S.remove(A[start]);
+            start++;
         }
     }
     return min_days;
